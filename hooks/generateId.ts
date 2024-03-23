b1df0319df4dab8e45fc43d72f00323fce1aecae
Alabi Olalekan Emmanuel <alabilekanemmanuel@gmail.com>
2024-03-23 21:29:16 +0100

    Delete hooks directory

--- hooks/generateId.ts
+++
@@ -1,7 +0,0 @@
-// Generate a unique comment ID
-export const generateCommentId = () => {
-  const timestamp = Date.now();
-  const random = Math.floor(Math.random() * 1000); 
-
-  return `${timestamp}-${random}`;
-}
