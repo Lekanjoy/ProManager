0bb4b63f66f0a248fc6ccf05f0cabde7d268454a
Lekanjoy <alabilekanemmanuel@gmail.com>
2024-06-26 18:48:52 +0530

    "Refactor getTeamData hook to use dynamic Supabase client and simplify team filtering logic"

--- hooks/getTeamData.ts
+++ hooks/getTeamData.ts
@@ -2,36 +2,27 @@ import { teamData } from "@/types";
 import { createClient } from "@/utils/supabase/client";
 import { PostgrestResponse, User } from "@supabase/supabase-js";
 
-const supabase = createClient();
-
 export async function getTeamData(
   user: User | null
 ): Promise<teamData | undefined> {
+  const supabase = createClient();
+
   try {
-    const { data: allTeams, error: teamsError }: PostgrestResponse<teamData> =
-      await supabase.from("teams").select("*");
+    const { data: filteredTeam, error: teamsError } = (await supabase
+      .from("teams")
+      .select("*")
+      .or(
+        `admin_id.eq.${user?.id},team_member.cs.${JSON.stringify([user?.id])}`
+      )) as PostgrestResponse<teamData>;
 
     if (teamsError) {
       throw teamsError;
     }
 
-    if (!allTeams) {
+    if (!filteredTeam) {
       console.warn("No teams found in the database.");
       return;
     }
-
-    const filteredTeam = allTeams.filter((team) => {
-      return (
-        team.admin_id === user?.id ||
-        (team.team_member && team.team_member.includes(user?.id as string))
-      );
-    });
-
-    if (!filteredTeam.length) {
-      console.warn("The user is not an admin or a team member.");
-      return;
-    }
-
     return filteredTeam[0];
   } catch (error) {
     if (error) {
